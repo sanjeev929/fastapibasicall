c09b06268511338f4f7d14388c75fee49efbedfc
Sanjeev <123805942+sanjeev929@users.noreply.github.com>
2024-04-19 09:16:19 +0530

    sansj

--- main.py
+++ main.py
@@ -30,17 +30,32 @@ async def userpost(user:models.User):
     return {"message":"successfull"}
 
 @app.put("/users/{user_id}")
-async def update_user(user_id: str, user: models.User):
-    result = await collection.update_one({"_id": user_id}, {"$set": user.dict()})
+async def update_user(user_id: int, user: models.User):
+    # Check if the user exists
+    existing_user = await collection.find_one({"id": user_id})
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail="User1 not found")
+
+    # Update the user information
+    result = await collection.update_one({"id": user_id}, {"$set": user.dict()})
     if not result.modified_count:
         raise HTTPException(status_code=404, detail="User not found")
+
     return {"message": "User updated successfully"}
 
+
 @app.delete("/users/{user_id}")
-async def delete_user(user_id: str):
-    result = await collection.delete_one({"_id": user_id})
+async def delete_user(user_id: int):
+    # Check if the user exists
+    existing_user = await collection.find_one({"id": user_id})
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Delete the user
+    result = await collection.delete_one({"id": user_id})
     if not result.deleted_count:
         raise HTTPException(status_code=404, detail="User not found")
+
     return {"message": "User deleted successfully"}
 
 
